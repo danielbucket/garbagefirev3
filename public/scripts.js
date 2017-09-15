84093c83a357004e1c8dfdeac374525be974bf0b
Daniel Bucket <danieljbucket@gmail.com>
2017-09-14 18:55:50 -0600

    this is what its like to fail

--- public/scripts.js
+++ public/scripts.js
@@ -1,32 +1,19 @@
 
-
 const mapItemStateOptionsToPage = item => {
 		$('#conditions').append(
 			`<button id=${item.id} class="conditionOption">${item.cleanliness}</button>`
 			)
 }
 
-const printItemsState = data => {
-	data.map(i => {
-		mapItemStateOptionsToPage(i)
-	})
-}
 
-const populateConditions = () => {
-	fetch('/api/v1/itemstate/')
-	.then(resp => resp.json())
-	.then(data => {
-			printItemsState(data.conditions)
-	})
-}
 
 const mapGarageItemsToPage = data => {
 	$('#garageItems').append(
 		`
 	<div class="garageItemCard">
-		<h3 class="garageItemName">${data.name}</h3>
-		<p class="garageItemReason">${data.excuse}<p>
-		<p class="garageItemCondition">${data.item_state_id}</p>
+		<p class="garageItemName garageDetail">${data.name}</p>
+		<p class="garageItemReason garageDetail">${data.excuse}<p>
+		<p class="garageItemCondition garageDetail">${data.item_state_id}</p>
 	</div>
 		`
 		)
@@ -72,6 +59,5 @@ $('#submitBtn').on('click', () => {
 
 
 $(document).ready(() => {
-	populateConditions()
 	populateGarage()
 })

7650c0a417fde715ee6d1f8510ea68b78576d14f
Daniel Bucket <danieljbucket@gmail.com>
2017-09-14 18:31:30 -0600

    well I got a list of all the items pringting t the page. gimme just another 10 hour and maybe I can get this done

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,77 @@
-$('#submit-btn').on('click', () => {
+
+
+const mapItemStateOptionsToPage = item => {
+		$('#conditions').append(
+			`<button id=${item.id} class="conditionOption">${item.cleanliness}</button>`
+			)
+}
+
+const printItemsState = data => {
+	data.map(i => {
+		mapItemStateOptionsToPage(i)
+	})
+}
+
+const populateConditions = () => {
+	fetch('/api/v1/itemstate/')
+	.then(resp => resp.json())
+	.then(data => {
+			printItemsState(data.conditions)
+	})
+}
+
+const mapGarageItemsToPage = data => {
+	$('#garageItems').append(
+		`
+	<div class="garageItemCard">
+		<h3 class="garageItemName">${data.name}</h3>
+		<p class="garageItemReason">${data.excuse}<p>
+		<p class="garageItemCondition">${data.item_state_id}</p>
+	</div>
+		`
+		)
+}
+
+const printGarageItems = data => {
+		mapGarageItemsToPage(data)
+}
+
+const fetchCondition = item => {
+	item.data.forEach(card => {
+		const id = card.item_state_id;
+
+		fetch(`/api/v1/itemstate/${id}`)
+		.then(resp => resp.json())
+		.then(list => {
+			printGarageItems(
+				Object.assign(card, {item_state_id:list.data[0].cleanliness})
+				)
+		})
+		.catch(error => console.log(error))
+	})
+}
+
+const populateGarage = () => {
+
+	fetch('/api/v1/items')
+	.then(resp => resp.json())
+	.then(returnValue => {
+		fetchCondition(returnValue)
+	})
+	.catch(error => console.log(error))
+}
+
+
+$('.conditionOption').on('click', () => {
 	console.log('tits')
+})
+
+$('#submitBtn').on('click', () => {
+	console.log('tits')
+})
+
+
+$(document).ready(() => {
+	populateConditions()
+	populateGarage()
 })
--- server/server.js
+++ server/server.js
@@ -7,22 +7,45 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const db = require('knex')(configuration)
 
-app.set('port', process.env.PORT || 3639)
+app.set('port', process.env.PORT || 3636)
 
+app.use(express.static(__dirname + '/../public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:true }))
-app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + '/../public/'))
+	res.sendFile(path.join(__dirname + '/../public/index.html'))
 })
 
+// DELETE ITEM FROM DB
+app.delete('/api/v1/items/destroy', (req, res) => {
+	const id = req.body.id;
+	db('items').where('id', id).del()
+	.then(() => res.status(202).json({ destroyed:`${id} has been deleted`}))
+})
 
+// GET ITEMS_STATE
+app.get('/api/v1/itemstate', (req,res) => {
+	db('items_state').select()
+	.then(conditions => res.status(200).json({ conditions }))
+	.catch(error => res.status(500).json({ error }))
+})
+
+// GET ITEMSTATE BY ID
+app.get('/api/v1/itemsstate/:id', (req,res) => {
+	const id = req.params.id;
+
+	db('items_state').where('id', id).select('cleanliness')
+	.then(stateID => {
+		res.status(200).json({ stateID })
+	})
+})
 
 
 // GET ALL ITEMS
 app.get('/api/v1/items', (req,res) => {
-	db('items').select()
+
+	db('items').select('*')
 	.then(data => res.status(200).json({ data }))
 	.catch(error => res.status(500).json({ error }))
 })
@@ -40,11 +63,12 @@ app.post('/api/v1/items', (req,res) => {
 app.get('/api/v1/itemstate/:id', (req,res) => {
 	id = req.params.id
 
-	db('items').where('item_state_id', id).select()
+	db('items_state').where('id', id).select()
 	.then(data => res.status(200).json({ data }))
-	.catch(error => res.status(500).json({ data }))
+	.catch(error => res.status(500).json({ error }))
 })
 
+//	GET ITEMS BY NAME
 app.get('/api/v1/items/:name', (req,res) => {
 	const name = req.params.name
 
@@ -53,6 +77,7 @@ app.get('/api/v1/items/:name', (req,res) => {
 	.catch(error => res.status(500).json({ data }))
 })
 
+// PUT ITEM
 app.put('/api/v1/items', (req,res) => {
 	const id = req.body.id;
 	const newPut = req.body.replaceWith
@@ -63,6 +88,8 @@ app.put('/api/v1/items', (req,res) => {
 	.catch(error => res.status(500).json({ error }))
 })
 
+
+// PATCH ITEM
 app.patch('/api/v1/items', (req,res) => {
 	const id = req.body.id;
 	const toUpdate = Object.keys(req.body.updateWith)[0]

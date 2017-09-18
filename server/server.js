6ecf31871810ab62ffd186fa198121dad4b9a62d
Daniel Bucket <danieljbucket@gmail.com>
2017-09-18 07:47:48 -0600

    change from node.env.DATABASE_URL to node.env.DATABASE_URL

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
 
-const environment = process.env.NODE_ENV || 'development'
+const environment = process.env.DATABASE_URL|| 'development'
 const configuration = require('../knexfile')[environment]
 const db = require('knex')(configuration)
 
